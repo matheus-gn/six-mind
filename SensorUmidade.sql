12f40b6a03cfe2abd07de256efcc8753e314d9e0
arthur-assis <61800677+arthur-assis@users.noreply.github.com>
2020-04-11 16:59:54 -0300

    Atualização do Banco de Dados
    Aprimoramento das funcionalidade de interação do usuário com sua plantação e seus recursos (manutenção de sensores), mais conexão e criação de tabelas.

--- SensorUmidade.sql
+++ SensorUmidade.sql
@@ -37,21 +37,49 @@ create table cadastro_adm -- Site cadastro adm's
  ('Zigon', MD5('12345678')),
  ('Vicente', MD5('12345678'));
 
-create table dados_sensor -- Dados do sensor expostos no site
+create table corpo_sensor -- Dados do sensor exposto no site
+(
+	idsensor int primary key auto_increment,
+	plantatipo varchar (50),
+	sensorlocal varchar (50),
+    estado char (1),
+    check (estado = 'S' or estado = 'N')
+) default charset = utf8;
+
+insert into corpo_sensor
+(plantatipo, sensorlocal, estado)
+values
+('Soja', 'A1', 'S'),
+('Soja', 'A1', 'N'),
+('Soja', 'A1', 'S'),
+('Soja', 'A1', 'S'),
+('Feijão', 'A2', 'S'),
+('Alface', 'A3', 'S');
+
+create table dados_planta -- Dados da planta expostos no site
 (
 	iddados int primary key auto_increment,
-    plantatipo varchar (50),
-    qtdgrao int,
-    sensorarea varchar (50),
-    qtdsensor int,
     temperatura double,
     umidade double,
-    datatempo datetime
+    datatempo datetime,
+    fksensor int,
+    foreign key (fksensor)
+    references corpo_sensor (idsensor)
 )default charset = utf8;
 
-insert into dados_sensor
-(plantatipo, qtdgrao, sensorarea, qtdsensor, temperatura, umidade, datatempo)
-VALUES
-('Soja', '1000000', '1°Ectar', '30', '20.0', '15', '2020-04-06 18:46:00');
+insert into dados_planta
+(temperatura, umidade, datatempo, fksensor)
+values
+('20.0', '60', '2020-04-06 18:46:00', 1),
+('21.0', '15', '2020-04-03 18:46:00', 2),
+('19.8', '70', '2020-04-06 18:46:00', 3),
+('20.5', '80', '2020-04-06 18:46:00', 6);
 
-select * from DADOSSENSOR;
+select * from cadastro_cliente;
+select * from cadastro_adm;
+select * from corpo_sensor;
+select * from dados_planta;
+select * from corpo_sensor, dados_planta
+where fksensor = idsensor;
+select * from corpo_sensor, dados_planta
+where fksensor = idsensor and plantatipo = 'Alface';
